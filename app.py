a6676f929bc6a1d25f0ef76de306b3ac045e874d
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-03-14 22:06:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 menu = """
 <a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>"""
 
-indice = """<img src="https://cdn-dejpn.nitrocdn.com/uxismCAJKdZklcCeScRYXbxxVTZIsrib/assets/static/optimized/rev-58a7024/wp-content/uploads/2021/08/FANI-slider-02.png">"""
-foto = """<img src="https://pbs.twimg.com/profile_images/1623393254153543686/SiIQ1MKu_400x400.jpg">"""
-imagem = """<img src="https://media.tenor.com/QPDDG_qlvKkAAAAC/tata-werneck-trolala.gif">"""
+indice = """<br><img src="https://cdn-dejpn.nitrocdn.com/uxismCAJKdZklcCeScRYXbxxVTZIsrib/assets/static/optimized/rev-58a7024/wp-content/uploads/2021/08/FANI-slider-02.png"><br>"""
+foto = """<br><img src="https://pbs.twimg.com/profile_images/1623393254153543686/SiIQ1MKu_400x400.jpg"><br>"""
+imagem = """<br><img src="https://media.tenor.com/QPDDG_qlvKkAAAAC/tata-werneck-trolala.gif"><br>"""
 
 
 @app.route("/")
