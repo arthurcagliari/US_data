d102cd711f4d7f5ccdc6ab2806904e460b33f6e5
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-03-14 21:45:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = """
-<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
-<br>
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>"""
 
-<img src="https://pbs.twimg.com/profile_images/1623393254153543686/SiIQ1MKu_400x400.jpg">
-"""
+indice = """<img src="https://gifs.eco.br/wp-content/uploads/2022/11/gifs-do-smilinguido-0.gif">""""
+sobre = """<img src="https://pbs.twimg.com/profile_images/1623393254153543686/SiIQ1MKu_400x400.jpg">""""
+contato = """<img src="https://media.tenor.com/QPDDG_qlvKkAAAAC/tata-werneck-trolala.gif">""""
+
 
 @app.route("/")
 def index():
-  return menu + "Olá, mundo! Esse é meu site. (Arthur Cagliari)"
+  return menu + "Olá, mundo! Esse é meu site. (Arthur Cagliari)" + indice
 
 @app.route("/sobre")
 def sobre():
-  return menu + "Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre" + sobre
 
 @app.route("/contato")
 def contato():
-  return menu + "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato" + contato
 
 
