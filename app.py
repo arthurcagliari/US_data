92eb3240d0cb212b5eb94de08c25fd684e63716d
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-03-20 20:45:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ menu = """
 <br><a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a><br>"""
 
 indice = """<br><img src="https://cdn-dejpn.nitrocdn.com/uxismCAJKdZklcCeScRYXbxxVTZIsrib/assets/static/optimized/rev-58a7024/wp-content/uploads/2021/08/FANI-slider-02.png"><br>"""
-foto = """<br><img src="https://pbs.twimg.com/profile_images/1623393254153543686/SiIQ1MKu_400x400.jpg"><br>"""
 imagem = """<br><img src="https://media.tenor.com/QPDDG_qlvKkAAAAC/tata-werneck-trolala.gif"><br>"""
 
 
@@ -16,7 +15,7 @@ def index():
 
 @app.route("/sobre")
 def sobre():
-  return menu + "Aqui vai o conteúdo da página Sobre" + foto
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
