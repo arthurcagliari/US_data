5fa020f8e0252ff3d89d122265f3ae4ace36b307
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-03-20 21:48:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def promocoes2():
 @app.route("/dedoduro")
 def dedoduro():
   mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
-  request.post(f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage', data=mensagem)
+  requests.post(f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage', data=mensagem)
   return "Mensagem enviada!"
