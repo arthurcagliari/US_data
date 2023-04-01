372528f586358b0aba6069862465488f09a36be5
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-01 19:38:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def telegram_bot():
   message = update["message"]["text"]
   nova_mensagem = {"chat_id": chat_id, "text": message}
   requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  return "ok")
+  return "ok"
