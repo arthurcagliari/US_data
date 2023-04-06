1b58b25d1a5bf6e4fc51f8fd88229f7b65239a7d
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-06 14:44:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import datetime
 import gspread
+import json
+import openai
 import os
+import pandas as pd
+import prettytable
 import requests
+
+from bs4 import BeautifulSoup as bs
 from flask import Flask, request
 from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
@@ -18,78 +25,43 @@ planilha = api.open_by_key("1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM")
 sheet = planilha.worksheet("Sheet1")
 app = Flask(__name__)
 
-def ultimas_promocoes():
-  scraper = ChannelScraper()
-  contador = 0
-  resultado = []
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    resultado.append(f"{message.created_at} {texto}")
-    if contador == 10:
-      return resultado
-
-    
-menu = """
-<a href="/">Página inicial</a> | <a href="/promocoes">Promoções</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
-<br>
-"""
-
-indice = """<br><img src="https://cdn-dejpn.nitrocdn.com/uxismCAJKdZklcCeScRYXbxxVTZIsrib/assets/static/optimized/rev-58a7024/wp-content/uploads/2021/08/FANI-slider-02.png"><br>"""
-imagem = """<br><img src="https://media.tenor.com/QPDDG_qlvKkAAAAC/tata-werneck-trolala.gif"><br>"""
-
-
 @app.route("/")
 def index():
-  return indice + menu + "Olá, mundo! Esse é meu site. (Arthur Cagliari)"
+  return indice + menu + "Olá, esse é um site de teste."
 
-@app.route("/sobre")
-def sobre():
-  return menu + "Aqui vai o conteúdo da página Sobre"
-
-@app.route("/contato")
-def contato():
-  return menu + "Aqui vai o conteúdo da página Contato" + imagem
-
-@app.route("/promocoes")
-def promocoes():
-  conteudo = menu + """
-  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
-  <br>
-  <ul>
-  """
-  for promocao in ultimas_promocoes():
-    conteudo += f"<li>{promocao}</li>"
-  return conteudo + "</ul>"
-
-
-@app.route("/promocoes2")
-def promocoes2():
-  conteudo = menu + """
-  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
-  <br>
-  <ul>
-  """
-  scraper = ChannelScraper()
-  contador = 0
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    conteudo += f"<li>{message.created_at} {texto}</li>"
-    if contador == 10:
-      break
-  return conteudo + "</ul>"
-
-@app.route("/dedoduro")
-def dedoduro():
-  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
-  requests.post(f'https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage', data=mensagem)
-  return "Mensagem enviada!"
-
-@app.route("/dedoduro2")
-def dedoduro2():
-  sheet.append_row(["Arthur", 33, "Sorocabano", "Jornalista"])
-  return "Planilha escrita!"
+@app.route("/raspagem_US")
+def raspagem():
+  GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+  with open("credenciais.json", mode="w") as arquivo:
+    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+  conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+  api = gspread.authorize(conta)
+  planilha = api.open_by_key("1S_ztKSv_gjalYZCjrb5CvU1fQMjHEfLw1k9i50HomF8")
+  sheet = planilha.worksheet("US_Data")
+  linhas = sheet.get("A3:Q20")
+  
+  headers = {'Content-type': 'application/json'}
+  data = json.dumps({"seriesid": ['CUSR0000SA0','CUUR0000SA0', 'CUSR0000SA0L1E','CUUR0000SA0L1E','WPSFD4','WPUFD4', 'WPSFD49104','WPUFD49104', 'CES0000000001','LNS14000000','CES0500000003'],"startyear":"2021", "endyear":"2023"})
+  p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+  json_data = json.loads(p.text)
+  for series in json_data['Results']['series']:
+    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+        footnotes=""
+        for footnote in item['footnotes']:
+            if footnote:
+                footnotes = footnotes + footnote['text'] + ','
+        if 'M01' <= period <= 'M12':
+            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+    output = open(seriesId + '.txt','w')
+    output.write (x.get_string())
+    output.close() 
+   dados = p.json()
+  return "right"
 
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
@@ -98,7 +70,7 @@ def telegram_bot():
   with open("credenciais.json", mode="w") as arquivo:
     arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
   conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
-  api = gspread.authorize(conta) # sheets.new
+  api = gspread.authorize(conta)
   planilha = api.open_by_key("1S_ztKSv_gjalYZCjrb5CvU1fQMjHEfLw1k9i50HomF8")
   sheet = planilha.worksheet("US_Data")
   linhas = sheet.get("A3:Q20")
