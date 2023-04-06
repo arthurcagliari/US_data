789866943a7ca7818aa57eff209545037073443d
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-06 14:47:52 -0300

    Create scraper_US.py

---
+++ scraper_US.py
@@ -0,0 +1,11 @@
+import datetime
+import getpass
+import gspread
+import json
+import openai
+import pandas as pd
+import prettytable
+import requests
+from bs4 import BeautifulSoup as bs
+from oauth2client.service_account import ServiceAccountCredentials
+
