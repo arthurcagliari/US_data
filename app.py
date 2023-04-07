f590114fa6114b8bfb885d7f4fe05892d58e171e
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-07 14:04:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
 from scraper import payroll
 from scraper import texto_inf
-from update import CPI_PPI
+from update import CPI_PPI, payroll, renda, mes, beige_book
 
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
@@ -71,58 +71,10 @@ def raspagem():
   dados = p.json()
   lista_CPI = CPI_PPI(0,1,2,3)
   lista_PPI = CPI_PPI(4,5,6,7)
+  lista_vagas = payroll()
+  lista_ganho = renda()
   
-    ### Primeiro, começando pela definição do número total de vagas geradas:
-  dados = p.json()
-  mes_atual_mt = dados['Results']['series'][8]['data'][0]['value']
-  mes_anterior_mt = dados['Results']['series'][8]['data'][1]['value']
-  mes_antes_anterior_mt = dados['Results']['series'][8]['data'][2]['value']
-  total_de_vagas = '%.0f' %((float(mes_atual_mt) - float(mes_anterior_mt)))
-  total_de_vagas_antes = '%.0f' %((float(mes_anterior_mt) - float(mes_antes_anterior_mt))) 
-
-    ### Segundo, descobrindo a taxa de desemprego do mês:
-  taxa_desemprego_atual = dados['Results']['series'][9]['data'][0]['value']
-  taxa_desemprego_anterior = dados['Results']['series'][9]['data'][1]['value']
-
-    ### Terceiro, calculando o ganho por hora na relação mensal e relação anual
-  ganho_atual = dados['Results']['series'][10]['data'][0]['value']
-  ganho_anterior = dados['Results']['series'][10]['data'][1]['value']
-  ganho_antes_ant = dados['Results']['series'][10]['data'][2]['value']
-  ganho_12anterior = dados['Results']['series'][10]['data'][12]['value']
-  ganho_13anterior = dados['Results']['series'][10]['data'][12]['value']
-  ganho_perc_mes = '%.1f' % ((float(ganho_atual) - float(ganho_anterior))*100/float(ganho_anterior))
-  ganho_perc_mes_ant = '%.1f' % ((float(ganho_anterior) - float(ganho_antes_ant))*100/float(ganho_antes_ant))
-  ganho_perc_ano = '%.1f' % ((float(ganho_atual) - float(ganho_12anterior))*100/float(ganho_12anterior))
-  ganho_perc_ano_ant = '%.1f' % ((float(ganho_anterior) - float(ganho_13anterior))*100/float(ganho_13anterior))
-  
-    ### definição do mês
-
-  def mes(p,n):
-      if dados['Results']['series'][p]['data'][n]['period'] == 'M01':
-        mes_1 = 'janeiro'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M02':
-        mes_1 = 'fevereiro'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M03':
-        mes_1 = 'março'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M04':
-        mes_1 = 'abril'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M05':
-        mes_1 = 'maio'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M06':
-        mes_1 = 'junho'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M07':
-        mes_1 = 'julho'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M08':
-        mes_1 = 'agosto'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M09':
-        mes_1 = 'setembro'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M10':
-        mes_1 = 'outubro'
-      elif dados['Results']['series'][p]['data'][n]['period'] == 'M11':
-        mes_1= 'novembro'
-      else:
-        mes_1 = 'dezembro'
-      return mes_1
+  ### definição do mês
 
   #p = 0 ---> dado do CPI
   #p = 4 ---> dado do PPI
@@ -159,67 +111,11 @@ def raspagem():
   lista_vazia = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
   lista_payroll = ["payroll (merc. de trabalho)", "dado burto atual", "dado br. mês anterior", "dado br. dois meses antes", "total de vagas, em milhares", "total de vagas mês anterior, em milhares", "taxa de desemprego atual", "taxa de desemprego anterior"]
   lista_meses_pay = ["mês referência", mes0Pay, mes1Pay, mes2Pay, mes0Pay, mes1Pay, mes0Pay, mes1Pay, mes0Pay, mes1Pay]
-  lista_vagas = ["", mes_atual_mt, mes_anterior_mt, mes_antes_anterior_mt, total_de_vagas, total_de_vagas_antes, taxa_desemprego_atual, taxa_desemprego_anterior]
   lista_payroll2 = ["ganho salarial", "ganho atual bruto", "ganho anterior bruto", "ganho há dois meses bruto", "ganho bruto 12 meses", "ganho bruto 13 meses", "ganho perc. atual", "ganho perc. anterior", "ganho acu. 12", "ganho acu. 12 anterior"]
-  lista_ganho = ["", ganho_atual, ganho_anterior, ganho_antes_ant, ganho_12anterior, ganho_13anterior, ganho_perc_mes, ganho_perc_mes_ant, ganho_perc_ano, ganho_perc_ano_ant]
 
   sheet.append_rows([lista_titulos, lista_meses_CPI, lista_CPI, lista_vazia, lista_meses_PPI, lista_PPI, lista_vazia, lista_meses_pay, lista_payroll, lista_vagas, lista_vazia, lista_meses_pay, lista_payroll2, lista_ganho])
   
-  ### Até aqui, eu raspei as informações do site do BLS e joguei numa planilha. Agora vou usar o openai para fazer um resumo do Livro Bege
-  ### Para começar, eu decidi criar uma regra para atualizar a URL que for raspada para obter o texto que eu quero do Livro Bege.
-  ### As datas da divulgação do documento já são conhecidas, por isso ajuda a criar uma regra para o ajuste da URL
-
-  data_atual = datetime.datetime.now()
-  dia_atual = data_atual.day
-  mes_atual = data_atual.month
-
-  if mes_atual <= 4:
-    if dia_atual < 19:
-      n = '03'
-
-  elif mes_atual <= 5:
-    if dia_atual < 30:
-      n = '04'
-
-  elif mes_atual <= 7:
-    if dia_atual < 11:
-      n = '05'
-
-  elif mes_atual <= 9:
-    if dia_atual < 5:
-      n = '07'
-
-  elif mes_atual <= 10:
-    if dia_atual < 17:
-      n = '09'
-
-  elif mes_atual <= 11:
-    if dia_atual < 28:
-      n = '10'
-
-  url = f'https://www.federalreserve.gov/monetarypolicy/beigebook2023{n}.htm'
-  
-  soup = bs(requests.get(url).content, "html.parser")
-  abstract = soup.find("div", "col-xs-12 col-md-9").text
-  palavra_inicial = 'Overall Economic Activity'
-  palavra_final = 'Highlights by Federal Reserve District'
-  pos_inicial = abstract.find(palavra_inicial) + len(palavra_inicial) + 1
-  pos_final = abstract.find(palavra_final)
-  subtexto = abstract[pos_inicial:pos_final].strip()
-  
-  prompt = "Considere o seguinte subtexto:"
-  pauta = "\n Transforme este subtexto em uma notícia de dois parágrafos, com no máximo 400 caracteres."
-
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt= prompt + subtexto + pauta,
-    temperature=0.0,
-    max_tokens=500,
-    top_p=1.0, 
-    frequency_penalty=0.0,
-    presence_penalty=0.0
-  )
-  livro_bege = response["choices"][0]["text"]
+  livro_bege = beige_book()
   sheet.update_cell(18, 2, livro_bege.strip())
   return "right"
 
