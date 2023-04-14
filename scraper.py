6db8e425bff30b6bea9ca028fc00fc550326b139
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-13 22:46:57 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -206,7 +206,7 @@ def lista_per(x):
     CPI_1 = dados['Results']['series'][s]['data'][k]['value']
     CPI_2 = dados['Results']['series'][s]['data'][int(k)+12]['value']
     CPI_per = '%.1f' % ((float(CPI_1) - float(CPI_2))*100/float(CPI_2))
-    return str(CPI_per)
+    return CPI_per
   
   lista_CPI_per = []  
   for w in range(0,12):
@@ -215,7 +215,6 @@ def lista_per(x):
   return lista_CPI_per
 
 def meses(p):
-
   lista_meses_inf = []
   for f in range(0,12):
     mes_inflacao = dados['Results']['series'][p]['data'][f]['periodName']
