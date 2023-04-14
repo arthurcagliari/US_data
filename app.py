b2eaf0767b023b42c1027d21f78aa192df701604
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-14 07:57:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -172,18 +172,17 @@ def telegram_bot():
   livro_bege = linhas[15][1]
   
   ### ajustando tabelinha
-  
-  def delta(g,h):
+ def delta(g,h):
     lista_acumulado = []
     for n in range (1,13):
       wed = f'{linhas[g][n]} \u2192 {linhas[h][n]}%'
       lista_acumulado.append(wed)
     s = f'''{lista_acumulado[0]} \n{lista_acumulado[1]}
-    {lista_acumulado[2]} \n{lista_acumulado[3]}
-    {lista_acumulado[4]} \n{lista_acumulado[5]}
-    {lista_acumulado[6]} \n{lista_acumulado[7]}
-    {lista_acumulado[8]} \n{lista_acumulado[9]}
-    {lista_acumulado[10]} \n{lista_acumulado[11]}'''
+{lista_acumulado[2]} \n{lista_acumulado[3]}
+{lista_acumulado[4]} \n{lista_acumulado[5]}
+{lista_acumulado[6]} \n{lista_acumulado[7]}
+{lista_acumulado[8]} \n{lista_acumulado[9]}
+{lista_acumulado[10]} \n{lista_acumulado[11]}'''
     return s  
 
   acu_CPI = delta(17,18)
