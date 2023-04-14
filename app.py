6dccdb356b30b63b1a9dd699b675280387e24e5e
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-14 11:35:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,28 +47,6 @@ def raspagem():
   sheet = planilha.worksheet("US_Data")
   app = Flask(__name__)
   openai.api_key = OPENAI_KEY
-  
-  ## Raspando os dados dos EUA, pelo site www.bls.gov
-  headers = {'Content-type': 'application/json'}
-  data = json.dumps({"seriesid": ['CUSR0000SA0','CUUR0000SA0', 'CUSR0000SA0L1E','CUUR0000SA0L1E','WPSFD4','WPUFD4', 'WPSFD49104','WPUFD49104', 'CES0000000001','LNS14000000','CES0500000003'],"startyear":"2021", "endyear":"2023"})
-  p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
-  json_data = json.loads(p.text)
-  for series in json_data['Results']['series']:
-      x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
-      seriesId = series['seriesID']
-      for item in series['data']:
-          year = item['year']
-          period = item['period']
-          value = item['value']
-          footnotes=""
-          for footnote in item['footnotes']:
-              if footnote:
-                  footnotes = footnotes + footnote['text'] + ','
-          if 'M01' <= period <= 'M12':
-              x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-      output = open(seriesId + '.txt','w')
-      output.write (x.get_string())
-      output.close()
       
    ### Montando a estrutura para formação de texto, de acordo com os dados dos EUA
   dados = p.json()
@@ -82,7 +60,6 @@ def raspagem():
   #p = 0 ---> dado do CPI
   #p = 4 ---> dado do PPI
   #p = 8 ---> dado do payroll
-
 
   mes0CPI = mes(0,0)
   mes1CPI = mes(0,1)
