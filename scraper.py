5f3807e89053383db3833db6219d8ae0f55a7ad6
arthurcagliari <114265997+arthurcagliari@users.noreply.github.com>
2023-04-14 09:25:49 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -200,33 +200,3 @@ def beige_book():
   )
   livro_bege = response["choices"][0]["text"].strip()
   return livro_bege
-
-def lista_per(x):
-  if x == 1:
-    nome_indice = "CPI"
-  if x == 5:
-    nome_indice = "PPI"
-  def CPI_PPI_per(s,k):
-    CPI_1 = dados['Results']['series'][s]['data'][k]['value']
-    CPI_2 = dados['Results']['series'][s]['data'][int(k)+12]['value']
-    CPI_per = '%.1f' % ((float(CPI_1) - float(CPI_2))*100/float(CPI_2))
-    return str(CPI_per)
-  
-  lista_CPI_per = []  
-  for w in range(0,12):
-    inflacao_per = CPI_PPI_per(x,w)
-    lista_CPI_per.append(inflacao_per)
-    if w == 11:
-      lista_CPI_per.insert(0, nome_indice)
-  return lista_CPI_per
-
-def meses(p):
-  lista_meses_inf = []
-  for f in range(0,12):
-    mes_inflacao = dados['Results']['series'][p]['data'][f]['periodName']
-    ano_inflacao = dados['Results']['series'][p]['data'][f]['year']
-    data_inf = f'{mes_inflacao[:3]}.{ano_inflacao[2:]}'
-    lista_meses_inf.append(data_inf)
-    if f == 11:
-      lista_meses_inf.insert(0, 'mês/referência')
-  return lista_meses_inf
